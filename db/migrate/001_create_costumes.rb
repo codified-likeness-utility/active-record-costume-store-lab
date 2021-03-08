1ffa66a3c7a25e7d932c3009191304815c9ce6de
Ian Davies <mr.daviesian@gmail.com>
2021-03-08 17:19:54 -0600

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,5 +1,17 @@
 # Create a class and inherit from ActiveRecord::Migration
+class CreateCostumes < ActiveRecord::Migration[4.2]
 
+    def change
+        create_table :costumes do |costume|
+            costume.string :name
+            costume.float :price
+            costume.string :image_url
+            costume.string :size
+            costume.timestamp :created_at,:updated_at
+        end
+    end
+
+end
 # By convention, the class name should match the part of the
 # file name after the number, so in this case:
 # 002_create_costumes.rb becomes class CreateCostumes
@@ -8,4 +20,4 @@
 # In this change method, create columns with the correct names and 
 # value types according to the spec
 
-# !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+# !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blrake
--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,3 +1,13 @@
-# Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
 
-# !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+    def change
+        create_table :costume_stores do |stores|
+            stores.string :name
+            stores.string :location
+            stores.integer :costume_inventory
+            stores.integer :num_of_employees
+            stores.boolean :still_in_business
+            stores.timestamp :opening_time,:closing_time
+        end
+    end
+end
